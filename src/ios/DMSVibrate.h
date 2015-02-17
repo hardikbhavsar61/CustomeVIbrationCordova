b3b3f4f1553a0922d77abd6ae304d15fc41b66fd
Hardik <hardikbhavsar61@gmail.com>
2015-02-17 11:42:28 +0530

    Create DMSVibrate.h

---
+++ src/ios/DMSVibrate.h
@@ -0,0 +1,13 @@
+//
+//  DMSVibrate.h
+//  DMSVibration
+//
+//  Created by Dynamic Methods  on 16/02/15.
+//
+//
+
+#import <Cordova/CDVPlugin.h>
+
+@interface DMSVibrate : CDVPlugin
+-(void)vibrate:(CDVInvokedUrlCommand*)command;
+@end
